6dc52765f48104aae8af8b61176da06404cd34da
jadie <jadeczjade008@gmail.com>
2026-02-27 17:47:35 +0100

    chore: player

---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	velocity = velocity.normalized() * speed
+	move_and_slide()
