73ad87f14fa0126ddd165cb264dd615b2d6d30a5
Arsen <minecraftmanager9@gmail.com>
2026-02-27 22:39:38 +0100

    fix srsen

--- scripts/background.gd
+++ scripts/background.gd
@@ -1,40 +1,31 @@
 extends TextureRect
 
-# List of background textures
 var backgrounds = [
-preload("res://IMGS/day.jpg"),
-preload("res://IMGS/eve.jpg"),
-preload("res://IMGS/nigg.jpg"),
-preload("res://IMGS/mor.jpeg")
+	preload("res://IMGS/day.jpg"),
+	preload("res://IMGS/eve.jpg"),
+	preload("res://IMGS/nigg.jpg"),
+	preload("res://IMGS/mor.jpeg")
 ]
-@export var transition_time: float = 1.5 # seconds for fade
 
-var current_index = 0
-var active_bg = true # true = BackgroundA visible, false = BackgroundB visible
+@export var transition_time := 1.5
+var index := 0
 
 func _ready():
-# Load the first background
-	$day.texture = load(backgrounds[current_index])
-	$eve.modulate.a = 0.0 # invisible
-	$Timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
+	texture = backgrounds[index]
+	modulate.a = 1.0
+	$Timer.timeout.connect(_on_timer_timeout)
 
-func _on_Timer_timeout():
-# Advance to next background
-	current_index += 1
-	if current_index >= backgrounds.size():
-		current_index = 0
+func _on_timer_timeout():
+	# fade out
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, transition_time)
 
-# Determine which TextureRect is fading in/out
-	var fade_bg = $eve if active_bg else $day
-	var show_bg = $day if active_bg else $eve
+	await tween.finished
 
-# Load new background to fade in
-	fade_bg.texture = load(backgrounds[current_index])
-	fade_bg.modulate.a = 0.0
+	# change texture
+	index = (index + 1) % backgrounds.size()
+	texture = backgrounds[index]
 
-# Animate fade
-	fade_bg.animate_property("modulate:a", 1.0, transition_time)
-	show_bg.animate_property("modulate:a", 0.0, transition_time)
-
-# Swap active background
-	active_bg = not active_bg
+	# fade in
+	var tween_in = create_tween()
+	tween_in.tween_property(self, "modulate:a", 1.0, transition_time)
