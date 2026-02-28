ac12dabbe783b2aed41b361513564685987874da
fenex <fenexcs@gmail.com>
2026-02-28 11:41:34 +0100

    made the fish spawner impossible to walk and fixed the bug i caused uwu

--- scripts/fish_spawner.gd
+++ scripts/fish_spawner.gd
@@ -15,16 +15,25 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("player"):
+		return
 	if not fish_spawned and body.is_in_group("player"):
-		spawn_fish.call_deferred()
-func spawn_fish() -> void:
+		spawn_fish.call_deferred(body)
+func spawn_fish(player) -> void:
 	if fish_spawned:
 		return
 	fish_spawned = true
+	var direction = (player.global_position - global_position).normalized()
+	var spawn_direction = Vector2.ZERO
+	if abs(direction.x) > abs(direction.y):
+		spawn_direction = Vector2.RIGHT if direction.x > 0 else Vector2.LEFT
+	else:
+		spawn_direction = Vector2.DOWN if direction.y > 0 else Vector2.UP
+
 	$AnimatedSprite2D.play("open")
 	print("spawn fish")
 	var fish = attack_fish_dash.instantiate()
-	fish.global_position = global_position
+	fish.global_position = global_position + spawn_direction
 	get_parent().add_child(fish)
 	# TODO
 	print("we got somewhere")
