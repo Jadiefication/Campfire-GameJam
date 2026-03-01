1c25b061ae0c5aa4ca0047ae7330261038f0b23a
jadie <jadeczjade008@gmail.com>
2026-03-01 01:10:23 +0100

    loading and saving

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -5,6 +5,8 @@ signal money_changed(new_amount)
 var money: int = 0:
 	set(value):
 		money = value
+		if value > 0:
+			total_money = value
 		money_changed.emit(money)
 		
 signal hp_changed(new_amount)
@@ -13,3 +15,5 @@ var hp: int = 100:
 	set(value):
 		hp = value
 		hp_changed.emit(value)
+		
+var total_money: int = 0
